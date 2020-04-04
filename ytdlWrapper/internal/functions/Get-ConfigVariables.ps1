f373585d6b4dd93ff63cec2d08b7e0bd7bcf721c
KubaP <prusakjakub@gmail.com>
2020-04-04 13:24:57 +0100

    Wrote variable parsing logic
    - Wrote variable parsing logic, both for names and scriptblock definitions.
    - Rewrote Get-ConfigVariable to work for input or variable definitions.

--- ytdlWrapper/internal/functions/Get-ConfigVariables.ps1
+++ ytdlWrapper/internal/functions/Get-ConfigVariables.ps1
@@ -1,4 +1,4 @@
-﻿function Get-ConfigVariables {
+﻿function Get-ConfigDefinitions {
 	<#
 	.SYNOPSIS
 		Short description
@@ -20,48 +20,88 @@
 		
 		[Parameter(Mandatory = $true)]
 		[String]
-		$Path
+		$Path,
+		
+		[Parameter()]
+		[switch]
+		$InputDefinitions,
+		
+		[Parameter()]
+		[switch]
+		$VariableDefinitions,
+		
+		[Parameter()]
+		[switch]
+		$VariableScriptblocks
 		
 	)
 	
-	# Read in the config file
-	$configFilestream = Get-Content -Path $Path
-			
-	$dynamicInputList = [System.Collections.Generic.List[System.String]]@()
+	# Read in the config file as a single string
+	$configFilestream = Get-Content -Path $Path -Raw
 	
-	# Iterate through all lines in the config file when regexing
-	foreach ($line in $configFilestream) {
+	$definitionList = [System.Collections.Generic.List[System.String]]@()
+	
+	if ($InputDefinitions -eq $true) {
 		
 		# Find all matches to:
-		# 1.	--some-parameter i@{description}	: normal parameter definition
-		# 1.	-s i@{description}					: shorthand parameter definition
-		# 2.	'i@{description}'					: special case for url, since it doesn't have a flag
+		# 1.	--some-parameter i@{name}	: normal parameter definition
+		# 1.	-s i@{name}					: shorthand parameter definition
+		# 2.	'i@{name}'					: special case for url, since it doesn't have a flag
 		# Also matches even if multiple parameter definitions are on the same line
-		$regex1 = [regex]::Matches($line, "(-(\S+)\s'?i@{(\w+)}'?)\s*")
-		$regex2 = [regex]::Matches($line, "('i@{(\w+)}')")
+		$regex1 = [regex]::Matches($configFilestream, "(-(\S+)\s'?i@{(\w+)}'?)\s*")
+		$regex2 = [regex]::Matches($configFilestream, "('i@{(\w+)}')")
 		
 		# Add the descriptor fields to the list
 		foreach ($match in $regex1) {
 			
 			# .Group[1] is the whole match
 			# .Group[2] is the 'some-parameter' or 's' match
-			# .Group[3] is the 'description' match
+			# .Group[3] is the 'name' match
 			
-			$dynamicInputList.Add($match.Groups[3].Value)
+			$definitionList.Add($match.Groups[3].Value)
 			
 		}
 		
 		foreach ($match in $regex2) {
 			
 			# .Group[1] is the whole match
-			# .Group[2] is the 'description' match
+			# .Group[2] is the 'name' match
 			
-			$dynamicInputList.Add($match.Groups[2].Value)
+			$definitionList.Add($match.Groups[2].Value)
+			
+		}
+		
+	}else {
+		
+		# Find all matches to:
+		# 1.	--some-parameter v@{name}{scriptblock}	: normal parameter definition
+		# 1.	-s v@{name}{scritpblock}				: shorthand parameter definition
+		# Also matches even if multiple parameter definitions are on the same line
+		$regex = [regex]::Matches($configFilestream, "(-(\S+)\s'?v@{(\w+)}{(.*?)}'?)\s+")
+		
+		# Add the descriptor fields to the list
+		foreach ($match in $regex) {
+			
+			# .Group[1] is the whole match
+			# .Group[2] is the 'some-parameter' or 's' match
+			# .Group[3] is the 'name' match
+			# .Group[4] is the 'scriptblock' match
+			
+			if ($VariableDefinitions -eq $true) {
+				
+				$definitionList.Add($match.Groups[3].Value)
+				
+			}elseif ($VariableScriptblocks -eq $true) {
+				
+				$definitionList.Add($match.Groups[4].Value)
+				
+			}
 			
 		}
 		
 	}
 	
-	Write-Output $dynamicInputList -NoEnumerate
+	# Return the list as a List object, rather than as an array (by default)
+	Write-Output $definitionList -NoEnumerate
 	
 }
