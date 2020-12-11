760d0c85b13f42afd0b6b8be346eb98612a7508b
KubaP <prusakjakub@gmail.com>
2020-12-11 20:32:06 +0000

    Add object classes
    - Job class containing necessary members.
    - Template class containing methods for getting the inputs and getting a completed file.

---
+++ ytdlWrapper/internal/classes/YoutubeDlJob.ps1
@@ -0,0 +1,14 @@
+﻿class YoutubeDlJob
+{
+	[string]$Name
+	[string]$Path
+	hidden [hashtable]$_Variables
+	
+	# Constructor.
+	YoutubeDlJob ([string]$name, [string]$path, [hashtable]$variableValues)
+	{
+		$this.Name = $name
+		$this.Path = $path
+		$this._Variables = $variableValues
+	}
+}
---
+++ ytdlWrapper/internal/classes/YoutubeDlTemplate.ps1
@@ -0,0 +1,61 @@
+﻿class YoutubeDlTemplate
+{
+	[string]$Name
+	[string]$Path
+	
+	# Constructor.
+	YoutubeDlTemplate([string]$name, [string]$path)
+	{
+		$this.Name = $name
+		$this.Path = $path
+	}
+	
+	[hashtable] GetInputs()
+	{
+		$inputs = New-Object -TypeName hashtable
+		
+		# If the config filepath is valid, get the definitions within the file.
+		if (Test-Path -Path $this.Path)
+		{
+			$inputDefinitions = Read-ConfigDefinitions -Path $this.Path -InputDefinitions
+			
+			foreach ($definition in $inputDefinitions)
+			{
+				$inputs[$definition] = ""
+			}
+		}
+		
+		return $inputs
+	}
+	
+	[string] CompleteTemplate([hashtable]$inputs)
+	{
+		# If the config filepath is invalid, return nothing.
+		if (-not (Test-Path -Path $this.Path))
+		{
+			return $null
+		}
+		
+		$configFilestream = Get-Content -Path $this.Path -Raw
+		$inputDefinitions = Read-ConfigDefinitions -Path $this.Path -InputDefinitions
+		foreach ($definition in $inputDefinitions)
+		{
+			if ($inputs.ContainsKey($definition))
+			{
+				
+				$configFilestream = $configFilestream -replace "i@{$definition}", $inputs[$definition]
+				
+			}
+			else
+			{
+				return $null
+			}
+		}
+		
+		$stream = [System.IO.MemoryStream]::new([byte[]][char[]]$configFilestream)
+		$hash = (Get-FileHash -InputStream $stream -Algorithm SHA256).hash
+		Out-File -FilePath "$script:Folder\$hash.conf" -Force -InputObject $configFilestream
+		return $hash
+	}
+	
+}
